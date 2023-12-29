ca652e21adcca07bf2ae220bd66a4cdd8a1b6289
Real-JP-Games <153140214+Real-JP-Games@users.noreply.github.com>
2023-12-29 17:16:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Wait for the DOM to be fully loaded before adding event listeners
 
-  var welcomeMessage = document.getElementById("welcome-message");
   var changeMessageBtn = document.getElementById("change-message-btn");
 
   if (changeMessageBtn) {
     changeMessageBtn.addEventListener("click", function () {
-      // Change the welcome message when the button is clicked
-      welcomeMessage.textContent = "Welcome to the Updated Test Page!";
+      // Navigate to the "second-page.html" file when the button is clicked
+      window.location.href = "second-page.html";
     });
   }
 });
